0a75c43c74519a65efbbeeffb910f00525ef1b73
Lokesh shinde <lokeshinde500@gmail.com>
2024-10-04 06:06:01 +0530

    cors fixing

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -4,7 +4,7 @@ import multer from "multer";
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
 
-  description: { type: String, required: true, unique: true, trim: true },
+  description: { type: String, required: true, trim: true },
 
   category: { type: String, required: true, trim: true },
 
