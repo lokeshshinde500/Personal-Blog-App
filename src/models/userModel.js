af813696a79669da71cdfe05c35de9c7d476c977
Lokesh shinde <lokeshinde500@gmail.com>
2024-10-04 05:41:23 +0530

    fix cors

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true, unique: true },
+    name: { type: String, required: true, trim: true },
 
     email: { type: String, required: true, unique: true },
 
